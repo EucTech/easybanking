4142670245922db7e0f67f97ffed05e2cbdb1dcf
Euccreative <euccreative@gmail.com>
2022-10-13 14:56:25 +0100

    nav

--- js/main.js
+++ js/main.js
@@ -17,3 +17,26 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+const navlinks = document.querySelector('.navlinks')
+const navlinksEl = document.querySelectorAll('.navlinks a')
+const navOpen = document.querySelector('.open')
+const navClose = document.querySelector('.close')
+
+function openNav(){
+  navlinks.style.top = "40%";
+  navOpen.style.display = "none"
+  navClose.style.display = "block"
+}
+
+function closeNav(){
+  navlinks.style.top = "-40%";
+  navOpen.style.display = "block"
+  navClose.style.display = "none"
+}
+
+navlinksEl.forEach(navLink => {
+  navLink.addEventListener('click', function(){
+    closeNav()
+  })
+})
